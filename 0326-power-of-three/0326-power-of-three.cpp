243a134225ce7bb8270414281b801335c393ce03
Lokesh8514 <lokesh22fhg@gmail.com>
2025-08-13 11:34:58 +0530

    Time: 4 ms (47.44%), Space: 9 MB (46.41%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+
     bool isPowerOfThree(int n) {
-        if(n==0)
-        return false;
-        double a = log(n) / log(3);
-        return fabs(a - round(a)) < 1e-10;
+        return n>0 && 1162261467 % n == 0;
+        // if(n==0)
+        // return false;
+        // double a = log(n) / log(3);
+        // return fabs(a - round(a)) < 1e-10;
 
         // double a=log(n)/log(3);
         // cout<<a;
