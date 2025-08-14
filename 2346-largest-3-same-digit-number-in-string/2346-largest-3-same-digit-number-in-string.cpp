7a944dc262ea1e6853011b6bac6c577fc6479cbe
Lokesh8514 <lokesh22fhg@gmail.com>
2025-08-14 14:42:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (22.96%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        // vector<string>s{"999","888","777","666","555","444","333","222","111","000"};
-        // for(int i=0;i<s.size();i++){
-        //     if(num.find(s[i])!=string::npos){
-        //         return s[i];
-        //     }
-        // }
-        // return "";
-
-        string maxi="";
-        for(int i=0;i<num.size()-2;i++){
-            if(num[i]==num[i+1]&&num[i+1]==num[i+2]){
-                string s=num.substr(i,3);
-                maxi=max(maxi,s);
+        vector<string>s{"999","888","777","666","555","444","333","222","111","000"};
+        for(int i=0;i<s.size();i++){
+            if(num.find(s[i])!=string::npos){
+                return s[i];
             }
         }
-        return maxi;
+        return "";
+
+        // string maxi="";
+        // for(int i=0;i<num.size()-2;i++){
+        //     if(num[i]==num[i+1]&&num[i+1]==num[i+2]){
+        //         string s=num.substr(i,3);
+        //         maxi=max(maxi,s);
+        //     }
+        // }
+        // return maxi;
     }
 };
