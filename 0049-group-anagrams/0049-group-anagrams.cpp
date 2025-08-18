2ae8890d4e9ce7f4cd60a69f94b2611e54bbdeca
Lokesh8514 <lokesh22fhg@gmail.com>
2025-08-18 11:41:06 +0530

    Time: 18 ms (52.54%), Space: 26.1 MB (37.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        for(string x:strs){
-            string word=x;
-            sort(word.begin(),word.end());
-            mp[word].push_back(x);
+        /*Declare an unordered_map to group strings by their sorted version
+        Key: Sorted string
+         Value: Vector of strings that are anagrams of each other*/
+        unordered_map<string, vector<string>> mp;
+        
+        // Iterate through each string in the input vector
+        for (auto x : strs) {
+            string word = x;              // Make a copy of the current string
+            sort(word.begin(), word.end()); // Sort the string alphabetically to create a unique key for anagrams
+            mp[word].push_back(x);        // Add the original string to the vector corresponding to the sorted key
         }
-        vector<vector<string>>ans;
-        for(auto x:mp){
-            ans.push_back(x.second);
+        
+        // Declare a result vector to store grouped anagrams
+        vector<vector<string>> ans;
+        for (auto x : mp) {
+            ans.push_back(x.second);      // Add each group of anagrams (values in the map) to the result
         }
-        return ans;
+        
+        return ans;                       
     }
-
 };
