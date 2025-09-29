a7b8ac98dbb676c7810da97f00f47a95e337c74b
Lokesh8514 <lokesh22fhg@gmail.com>
2025-09-29 18:42:56 +0530

    Time: 1 ms (78.66%), Space: 22.6 MB (33.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def rotate(self, nums, k):
         k%=len(nums)
-        # n=len(nums)
-        # print(nums[n-k:]+nums[:n-k])
-        # return {}
         n=len(nums)
-        nums[:]=nums[::-1]
-        part1=nums[:k]
-        part1.reverse()
-        nums[:k]=part1
-        part2=nums[k:n]
-        part2.reverse()
-        nums[k:n]=part2
-        print(nums)
+        nums[:]=nums[n-k:]+nums[:n-k]
         return nums
+        # n=len(nums)
+        # nums[:]=nums[::-1]
+        # part1=nums[:k]
+        # part1.reverse()
+        # nums[:k]=part1
+        # part2=nums[k:n]
+        # part2.reverse()
+        # nums[k:n]=part2
+        # print(nums)
+        # return nums
         """
         :type nums: List[int]
         :type k: int
