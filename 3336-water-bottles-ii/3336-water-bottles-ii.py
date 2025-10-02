96ee881bc1f13fc87dde24f4c30d972098a8167c
Lokesh8514 <lokesh22fhg@gmail.com>
2025-10-02 19:26:18 +0530

    Time: 12 ms (90.48%), Space: 12.3 MB (52.38%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.py
+++ 3336-water-bottles-ii/3336-water-bottles-ii.py
@@ -1,18 +1,25 @@
 class Solution(object):
-    def maxBottlesDrunk(self, numBottles, numExchange):
-        ans=numBottles
-        eb=numBottles
-        fb=0
-        while(eb>=numExchange or fb!=0):
-            if(eb<numExchange):
-                eb+=fb
-                fb=0
-            if(eb>=numExchange):
-                ans+=1
-                eb-=numExchange
-                numExchange+=1
-                fb+=1
+    def maxBottlesDrunk(self, numBottles, x):
+        #better version
+        ans = numBottles
+        while numBottles >= x:
+            numBottles -= x - 1
+            x += 1
+            ans += 1
         return ans
+        # ans=numBottles
+        # eb=numBottles
+        # fb=0
+        # while(eb>=numExchange or fb!=0):
+        #     if(eb<numExchange):
+        #         eb+=fb
+        #         fb=0
+        #     if(eb>=numExchange):
+        #         ans+=1
+        #         eb-=numExchange
+        #         numExchange+=1
+        #         fb+=1
+        # return ans
 
         """
         :type numBottles: int
