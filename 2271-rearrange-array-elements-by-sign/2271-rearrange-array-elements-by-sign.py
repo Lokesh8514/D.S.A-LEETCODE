37959f182d0c47c22d948ad536eaf6ef638537e9
Lokesh8514 <lokesh22fhg@gmail.com>
2025-10-03 12:47:12 +0530

    Time: 75 ms (69%), Space: 47.8 MB (39.83%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,25 +1,38 @@
 class Solution(object):
     def rearrangeArray(self, nums):
-        arrp=[]
-        arrn=[]
         n=len(nums)
+        pos=0
+        neg=1
+        ans=[0]*n
         for i in range(0,n):
-            if(nums[i]>0):
-                arrp.append(nums[i])
+            if(nums[i]<0):
+                ans[neg]=nums[i]
+                neg+=2
             else:
-                arrn.append(nums[i])
-        P=0
-        N=0
-        i=0
-        while(i<n):
-            if(i % 2==0):
-                nums[i]=arrp[P]
-                P+=1
-            else:
-                nums[i]=arrn[N]
-                N+=1
-            i+=1
-        return nums
+                ans[pos]=nums[i]
+                pos+=2
+        return ans
+        #uses twice the space needed
+        # arrp=[]
+        # arrn=[]
+        # n=len(nums)
+        # for i in range(0,n):
+        #     if(nums[i]>0):
+        #         arrp.append(nums[i])
+        #     else:
+        #         arrn.append(nums[i])
+        # P=0
+        # N=0
+        # i=0
+        # while(i<n):
+        #     if(i % 2==0):
+        #         nums[i]=arrp[P]
+        #         P+=1
+        #     else:
+        #         nums[i]=arrn[N]
+        #         N+=1
+        #     i+=1
+        # return nums
 
         """
         :type nums: List[int]
