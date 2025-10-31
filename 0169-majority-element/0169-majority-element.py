8de7a7c97ae4cabf49159fbb22e046bf4903fabf
Lokesh8514 <lokesh22fhg@gmail.com>
2025-10-31 16:29:33 +0530

    Time: 5 ms (76.17%), Space: 14.8 MB (6.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,30 @@
 class Solution(object):
     def majorityElement(self, nums):
-        mymap={}
+        count=1
+        candidate=nums[0]
         n=len(nums)
-        if(n==1):
-            return nums[0]
-
-        for i in range(0,n):
-            if nums[i] in mymap:
-                mymap[nums[i]]+=1
-                if(mymap[nums[i]]> n//2):
-                    return nums[i]
+        for i in range(1,n):
+            if nums[i]==candidate:
+                count+=1
             else:
-                mymap[nums[i]]=1
-        return -1
+                count-=1
+            if count==0:
+                candidate=nums[i]
+                count=1
+        return candidate
+        # mymap={}
+        # n=len(nums)
+        # if(n==1):
+        #     return nums[0]
+
+        # for i in range(0,n):
+        #     if nums[i] in mymap:
+        #         mymap[nums[i]]+=1
+        #         if(mymap[nums[i]]> n//2):
+        #             return nums[i]
+        #     else:
+        #         mymap[nums[i]]=1
+        # return -1
         """
         :type nums: List[int]
         :rtype: int
